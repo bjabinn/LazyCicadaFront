aa520cc9d81615dfe994d2f9c0b4498da98aba71
Daniel Gonzalez Morillo <dgonzmor@everis.com>
2018-10-31 11:38:47 +0100

    Styling calendar table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,17 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Daniel González Morillo', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Cristian Antonio González Robles', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 3, name: 'Ezequiel Pozo Fernandez', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Ismael Milan Márquez', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Jose Carlos Gadea Doncel', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Miguel Sanchez Infante', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Alfonso García Bellod', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'David Mateos', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'David Panal Cabrera', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Emilio Jose Palma Barroso', weight: 20.1797, symbol: 'Ne'},
+  {position: 11, name: 'Jorge Carlos Lozano Gomez', weight: 20.1797, symbol: 'Ne'},
+  {position: 12, name: 'Jose Antonio Beltrán Márquez', weight: 20.1797, symbol: 'Ne'},
+  {position: 13, name: 'Verónica Padua Castilleja', weight: 20.1797, symbol: 'Ne'},
 ];
 
 @Component({
